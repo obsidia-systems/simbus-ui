6f82afe9a93a1ec507d90fa3d55dd46d7033becf
Kevin Olvera <me@kevinolvera.com>
2026-04-13 19:14:00 -0600

    chore: integrate Tailwind CSS into the project configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,11 +5,17 @@ import react from '@astrojs/react';
 
 import node from '@astrojs/node';
 
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
 
   adapter: node({
     mode: 'standalone'
-  })
+  }),
+
+  vite: {
+    plugins: [tailwindcss()]
+  }
 });
